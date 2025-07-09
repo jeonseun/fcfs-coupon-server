b1ddddbeb2183f9fabf4601fd6725033393ed375
mac-m1 <seun@mac-m1ui-MacBookAir.local>
2025-07-10 01:07:07 +0900

    init: spring boot 프로젝트 생성

---
+++ src/main/kotlin/me/seun/fcfscouponserver/FcfsCouponServerApplication.kt
@@ -0,0 +1,11 @@
+package me.seun.fcfscouponserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FcfsCouponServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<FcfsCouponServerApplication>(*args)
+}
---
+++ src/test/kotlin/me/seun/fcfscouponserver/FcfsCouponServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.seun.fcfscouponserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FcfsCouponServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
